4eb9ea864b8da03dc681dd7bf007f9b7081cba86
hady-achkar <hady.achkar@hotmail.com>
2022-12-23 13:28:34 +0100

    fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,3 @@ export {default as dbConnect} from './dbConnect'
 export {default as Stripe} from './stripe'
 export {default as S3Client} from './s3'
 export {default as SendEmail} from './emailJs'
-export {default as generatePassword } from './strongPassword';
-export {default as parceExcelData } from './xlsx';
